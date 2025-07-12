5565e2ba48edf50d4bae26713f8b20aba6f68ee8
Matheus Pissaia <pissaia.matheus03@gmail.com>
2025-07-11 22:18:47 -0300

    feat: add small test script

---
+++ tests/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "../api/api.h"
+
+int main(int argc, char **argv)
+{
+    char *write_data = "Hello World!";
+    const int size = strlen(write_data);
+
+    dsm_write(28, write_data, size);
+
+    char resp_data[size];
+    dsm_read(28, resp_data, size);
+
+    printf("Response data: %s\n", resp_data);
+
+    return 0;
+}
